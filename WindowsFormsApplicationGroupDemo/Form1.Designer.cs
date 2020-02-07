c86d90a5667827679b6a8eb4f704336a35b586b6
sanjay <sanjayranjeeth@gmail.com>
2020-02-07 08:26:56 +0200

    Added Hello World Message

--- WindowsFormsApplicationGroupDemo/Form1.Designer.cs
+++ WindowsFormsApplicationGroupDemo/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(467, 115);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(1213, 151);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "The Main Button";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2144, 1056);
+            this.Controls.Add(this.btnMain);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMain;
     }
 }
 
--- WindowsFormsApplicationGroupDemo/Form1.cs
+++ WindowsFormsApplicationGroupDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplicationGroupDemo
         {
             InitializeComponent();
         }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
